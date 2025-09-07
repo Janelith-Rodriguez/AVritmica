c355c223514fd443a85271fdbf574298544e8502
Janelith Rodriguez <janelithrodriguez@hotmail.com>
2025-09-07 16:35:57 -0300

    Agregar archivos de proyecto.

---
+++ AVritmica.Server/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+//--------------------------------------------------------------------------------------
+//configuracion de los servicios en el constructor de la aplicación
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
+
+
+
+//--------------------------------------------------------------------------------------
+//construccción de la aplicación
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
